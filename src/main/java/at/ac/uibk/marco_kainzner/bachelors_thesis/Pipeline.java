ddf504d3d5bb266f43f583edb30ec84d3bc8522c
Marco Kainzner <marco.kainzner@gmail.com>
2019-04-06 08:48:28 +0200

    Initial commit

---
+++ src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/Pipeline.java
@@ -0,0 +1,43 @@
+package at.ac.uibk.marco_kainzner.bachelors_thesis;
+
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.berkeleyparser.BerkeleyParser;
+import de.tudarmstadt.ukp.dkpro.core.io.tgrep.TGrepWriter;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import java.io.IOException;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReader;
+
+public class Pipeline {
+
+    public static void main(String[] args)
+            throws UIMAException, IOException {
+        CollectionReader reader = createReader(
+                TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, "src/main/resources",
+                TextReader.PARAM_LANGUAGE, "en",
+                TextReader.PARAM_PATTERNS, new String[]{"[+]*.txt"});
+
+        AnalysisEngineDescription seg = createEngineDescription(StanfordSegmenter.class);
+
+        AnalysisEngineDescription pos = createEngineDescription(StanfordPosTagger.class);
+
+        AnalysisEngineDescription ner = createEngineDescription(StanfordNamedEntityRecognizer.class);
+
+        AnalysisEngineDescription parser = createEngineDescription(BerkeleyParser.class,
+                BerkeleyParser.PARAM_WRITE_PENN_TREE, true);
+
+        AnalysisEngineDescription writer = createEngineDescription(TGrepWriter.class,
+                TGrepWriter.PARAM_TARGET_LOCATION, "out/penn-trees");
+
+        SimplePipeline.runPipeline(reader, seg, pos, ner, parser, writer);
+    }
+}

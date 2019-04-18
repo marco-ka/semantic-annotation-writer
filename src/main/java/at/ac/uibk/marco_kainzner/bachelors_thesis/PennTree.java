cd9af60b4c84dd28f7f95f0d2be29dadc7a11fad
Marco Kainzner <marco.kainzner@gmail.com>
2019-04-18 12:53:03 +0200

    Read rule from file

--- src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/PennTree.java
+++ src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/PennTree.java
@@ -4,13 +4,35 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.tregex.*;
 import edu.stanford.nlp.trees.PennTreeReader;
 
-import java.io.*;
+//import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class PennTree {
     public static void main(String[] args) throws IOException {
         String file = args[0];
         Tree tree = read(file);
-        printLocations(tree);
+        String rulePath = "C:\\Users\\Marco\\Documents\\Projects\\dkpro-pipeline\\src\\main\\resources\\longRule.txt";
+        matchRuleFromFile(tree, rulePath);
+//        printLocations(tree);
+    }
+
+    public static void matchRuleFromFile(Tree tree, String ruleFile) throws IOException {
+        String rule = readFile(ruleFile);
+        printMatches(tree, rule);
+    }
+
+    private static String readFile(String path) throws IOException {
+        BufferedReader buf = new BufferedReader(new FileReader(path));
+        String line = buf.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        while(line != null){
+            sb.append(line).append("\n");
+            line = buf.readLine();
+        }
+        return sb.toString();
     }
 
     public static void printLocations(Tree tree) {
@@ -24,7 +46,8 @@ public class PennTree {
 
         while (m.find()) {
             Tree subTree = m.getMatch();
-            System.out.println(subTree.toString());
+
+            System.out.println(subTree.label());
         }
     }
 

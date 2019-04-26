9713eaae136b057295c19f30490037889e1e0b8c
Marco Kainzner <marco.kainzner@gmail.com>
2019-04-26 19:08:24 +0200

    Write wiktionary markers to file

--- src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/Wiktionary.java
+++ src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/Wiktionary.java
@@ -9,19 +9,24 @@ import de.tudarmstadt.ukp.jwktl.api.filter.WiktionaryEntryFilter;
 import de.tudarmstadt.ukp.jwktl.api.util.Language;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Wiktionary {
-    final static String DUMP_PATH = "C:\\Users\\Marco\\Documents\\Projects\\dkpro-pipeline\\src\\main\\resources\\wiktionary\\enwiktionary-20190320-pages-articles.xml.bz2";
-    final static String DB_DIR = "C:\\Users\\Marco\\Documents\\Projects\\dkpro-pipeline\\src\\main\\resources\\wiktionary\\berkeley-db";
+    final static String DUMP_PATH = "resources\\wiktionary\\enwiktionary-20190320-pages-articles.xml.bz2";
+    final static String DB_DIR = "resources\\wiktionary\\berkeley-db";
 
     final static String[] ACTOR_IDENTIFIERS = { " person ", " organization ", " body " };
     final static String[] SITUATION_IDENTIFIERS = { " act ", " action " };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         query();
     }
 
@@ -32,7 +37,7 @@ public class Wiktionary {
         JWKTL.parseWiktionaryDump(dumpFile, outputDirectory, true);
     }
 
-    public static void query() {
+    public static void query() throws IOException {
         System.out.println(new Date());
         File wiktionaryDirectory = new File(DB_DIR);
         IWiktionaryEdition wkt = JWKTL.openEdition(wiktionaryDirectory);
@@ -43,24 +48,43 @@ public class Wiktionary {
 
         System.out.println(new Date());
 
+
         Stream<IWiktionaryEntry> entries = StreamSupport.stream(wkt.getAllEntries(filter).spliterator(), false);
-        entries = entries
-            .limit(2000)
+
+        List<String> words = entries
+//            .limit(10000)
             .filter(entry -> {
                 boolean containsAct = false;
                 for (IWiktionarySense sense : entry.getSenses()) {
                     String definition = sense.getGloss().getPlainText();
                     if (containsAny(definition, ACTOR_IDENTIFIERS)) {
-                        System.out.println(entry.getWord());
-                        System.out.println(definition);
-                        System.out.println();
+
+//                        System.out.println(entry.getWord());
+//                        System.out.println(definition);
+//                        System.out.println();
                         containsAct = true;
                         break;
                     }
                 }
                 return containsAct;
-            });
-        System.out.println("English nouns containing \" act\": " + entries.count());
+            })
+            .map(entry -> entry.getWord())
+            .collect(Collectors.toList());
+
+        System.out.println("English nouns containing actor markers: " + words.size());
+
+        Writer w = new FileWriter("out/wiktionary.txt");
+
+        words.forEach(word -> {
+            try {
+                w.write(word);
+                w.write("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        w.close();
 
         System.out.println(new Date());
 

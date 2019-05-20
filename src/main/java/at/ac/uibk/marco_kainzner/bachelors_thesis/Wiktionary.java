e610bb9312200cbf324de9da99eaa980e4719ded
Marco Kainzner <marco.kainzner@gmail.com>
2019-05-20 10:45:59 +0200

    Fix wiktionary path

--- src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/Wiktionary.java
+++ src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/Wiktionary.java
@@ -2,10 +2,8 @@ package at.ac.uibk.marco_kainzner.bachelors_thesis;
 
 import de.tudarmstadt.ukp.jwktl.JWKTL;
 import de.tudarmstadt.ukp.jwktl.api.IWiktionaryEdition;
-import de.tudarmstadt.ukp.jwktl.api.IWiktionaryEntry;
 import de.tudarmstadt.ukp.jwktl.api.IWiktionarySense;
 import de.tudarmstadt.ukp.jwktl.api.PartOfSpeech;
-import de.tudarmstadt.ukp.jwktl.api.filter.WiktionaryEntryFilter;
 import de.tudarmstadt.ukp.jwktl.api.filter.WiktionarySenseFilter;
 import de.tudarmstadt.ukp.jwktl.api.util.Language;
 
@@ -14,20 +12,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Wiktionary {
-    private final static String DUMP_PATH = "resources\\wiktionary\\enwiktionary-20190320-pages-articles.xml.bz2";
-    private final static String DB_DIR = "resources\\wiktionary\\berkeley-db";
+    private final static String DUMP_PATH = "resources\\db\\wiktionary\\enwiktionary-20190320-pages-articles.xml.bz2";
+    private final static String DB_DIR = "resources\\db\\wiktionary\\berkeley-db";
 
     private final static String[] ACTOR_IDENTIFIERS = { " person ", " organization ", " body " };
-    private final static String[] SITUATION_IDENTIFIERS = { " act ", " acting ", " action ", " actions " };
+    private final static String[] SITUATION_IDENTIFIERS = { " act ", " acts ", " acting ", " action ", " actions " };
 
     public static void main(String[] args) throws IOException {
         saveMarkers("situation", PartOfSpeech.NOUN, SITUATION_IDENTIFIERS);
-        saveMarkers("artifact", PartOfSpeech.NOUN, SITUATION_IDENTIFIERS);
     }
 
     public static void parse(String pathIn, String pathOut) {
@@ -38,18 +34,16 @@ public class Wiktionary {
     }
 
     private static void saveMarkers(String markerName, PartOfSpeech pos, String[] identifiers) throws IOException {
-        System.out.println(new Date());
+        log("Wiktionary: Looking for '" +  markerName + "' markers ...");
         File wiktionaryDirectory = new File(DB_DIR);
         IWiktionaryEdition wkt = JWKTL.openEdition(wiktionaryDirectory);
 
         WiktionarySenseFilter filter = new WiktionarySenseFilter();
-//        WiktionaryEntryFilter filter = new WiktionaryEntryFilter();
         filter.setAllowedWordLanguages(Language.ENGLISH);
         filter.setAllowedPartsOfSpeech(pos);
 
         System.out.println(new Date());
 
-//        Stream<IWiktionaryEntry> entries = StreamSupport.stream(wkt.getAllEntries(filter).spliterator(), false);
         Stream<IWiktionarySense> senses = StreamSupport.stream(wkt.getAllSenses(filter).spliterator(), false);
 
         Set<String> words = new HashSet<>();
@@ -59,25 +53,10 @@ public class Wiktionary {
             }
         });
 
-//        List<String> words = entries
-//            .limit(50000)
-//            .filter(entry -> {
-//                boolean containsAct = false;
-//                for (IWiktionarySense sense : entry.getSenses()) {
-//                    String definition = sense.getGloss().getPlainText();
-//                    if (containsAny(definition, identifiers)) {
-//                        containsAct = true;
-//                        break;
-//                    }
-//                }
-//                return containsAct;
-//            })
-//            .map(IWiktionaryEntry::getWord)
-//            .collect(Collectors.toList());
+        log("Wiktionary: Entries containing markers for '" + markerName + "': " + words.size());
+        log("Wiktionary: Writing to file ...");
 
-        System.out.println("English nouns containing markers for '" + markerName + "': " + words.size());
-
-        Writer w = new FileWriter("out/wiktionary-" + markerName + ".txt");
+        Writer w = new FileWriter("out/wiktionary-markers_" + markerName + ".txt");
 
         words.forEach(word -> {
             try {
@@ -89,10 +68,14 @@ public class Wiktionary {
         });
 
         w.close();
-
-        System.out.println(new Date());
+        log("Wiktionary: Done writing");
 
         wkt.close();
+        log("Wiktionary: Done");
+    }
+
+    private static void log(String msg) {
+        System.out.println(new Date()+ ": " + msg);
     }
 
     private static boolean containsAny(String haystack, String[] needles) {

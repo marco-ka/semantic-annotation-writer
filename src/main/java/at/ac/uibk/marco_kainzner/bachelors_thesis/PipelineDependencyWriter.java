bfacb29e1723a2dbe885a480acc6e61ff2f59aa3
Marco Kainzner <marco.kainzner@gmail.com>
2019-07-30 10:28:07 +0200

    Get document id from jCas

--- src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/PipelineDependencyWriter.java
+++ src/main/java/at/ac/uibk/marco_kainzner/bachelors_thesis/PipelineDependencyWriter.java
@@ -1,5 +1,6 @@
 package at.ac.uibk.marco_kainzner.bachelors_thesis;
 
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.constituent.Constituent;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
@@ -12,48 +13,73 @@ import edu.stanford.nlp.trees.tregex.TregexPattern;
 import net.sf.extjwnl.JWNLException;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 
 public class PipelineDependencyWriter extends JCasConsumer_ImplBase {
     @Override
+    // TODO: Param: Target location
     public void process(JCas jCas) {
         try {
-            var rules = Rules.getAll();
+            var metaData = DocumentMetaData.get(jCas);
+            var docName = metaData.getDocumentId();
 
+            var rules = Rules.getAll();
+            var dir = "C:/Users/Marco/Documents/Projects/dkpro-pipeline/resources/";
+            var path = Paths.get(dir, docName + "_matches.txt");
+            writeAllText(path.toString(), "");
+
+            var sentenceIndex = 0;
             for (Sentence sentence : JCasUtil.select(jCas, Sentence.class)) {
-                log("Starting sentence: '" + sentence.getCoveredText() + "'");
-                log("Rule: '" + rules.get(0).getFirst() + "'");
+                sentenceIndex++;
+
+//                log("Starting sentence: '" + sentence.getCoveredText() + "'");
+//                log("Rule: '" + rules.get(0).getFirst() + "'");
+//                log("---< RULE>---");
+//                log(rules.get(0).getSecond());
+//                log("---</RULE>---");
 
                 log("Matches for constituency rule: \n---");
                 var matchesConstituency = getMatches(sentence, rules.get(0).getSecond())
                         .stream()
-                        .peek(System.out::println)
+//                        .peek(System.out::println)
                         .collect(Collectors.toList());
 
                 log("Matches for dependency rule: \n---");
                 var matches = matchesConstituency.stream()
                         .filter(pennTreeNode -> hasDependency(pennTreeNode, ".*"))
-                        .peek(System.out::println)
+//                        .peek(System.out::println)
                         .collect(Collectors.toList());
 
-                log(matchesConstituency.size() + " constituency matches");
-                log(matches.size() + " final matches");
-                log("---\nDone with sentence");
+//                log(matchesConstituency.size() + " constituency matches");
+//                log(matches.size() + " final matches");
+//                log("---\nDone with sentence");
+
+                for (PennTreeNode match : matches) {
+                    var txt = "sentence_" + sentenceIndex + ": " + match.toString();
+                    appendLine(path.toString(), txt);
+                }
             }
 
             log("All sentences done");
+
         } catch (IOException | JWNLException e) {
             log("Failed to get rules");
             e.printStackTrace();
@@ -83,7 +109,7 @@ public class PipelineDependencyWriter extends JCasConsumer_ImplBase {
     private static boolean hasDependency(PennTreeNode constituencyTree, String dependencyTypeRegex) {
         var dependencies = toDependencyTree(constituencyTree)
             .filter(d -> d.getDependencyType().matches(dependencyTypeRegex))
-            .peek(d -> System.out.println("Dependency matches regex ('" + dependencyTypeRegex + "'): " + dependencyStr(d)))
+//            .peek(d -> System.out.println("Dependency matches regex ('" + dependencyTypeRegex + "'): " + dependencyStr(d)))
             .collect(Collectors.toList());
         return !dependencies.isEmpty();
     }
@@ -131,5 +157,23 @@ public class PipelineDependencyWriter extends JCasConsumer_ImplBase {
     private static void log(String msg) {
         var timestamp = new Timestamp(System.currentTimeMillis());
         System.out.println(timestamp + ": " + msg);
+    }
+
+    private static void appendLine(String path, String text) throws IOException {
+        File file = new File(path);
+        FileWriter fw = new FileWriter(path, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(text);
+        bw.newLine();
+        bw.close();
+    }
+
+    private static void writeAllText(String path, String text) throws IOException {
+        File file = new File(path);
+        file.createNewFile();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(text);
+        writer.close();
     }
 }
